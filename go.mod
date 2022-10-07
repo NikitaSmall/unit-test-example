3e47bbaec2630114f4852471ac1b897f2762a6e2
NikitaSmall <nikitasosnov@yahoo.com>
2022-10-07 17:15:36 +0300

    add go mod

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/irahardianto/service-pattern-go
+
+go 1.17
+
+require (
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
+	github.com/go-chi/chi v1.5.4
+	github.com/jinzhu/gorm v1.9.16
+	github.com/stretchr/testify v1.8.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/smartystreets/goconvey v1.7.2 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- services/PlayerService_test.go
+++ services/PlayerService_test.go
@@ -10,19 +10,19 @@ import (
 )
 
 func TestGetScore(t *testing.T) {
-
 	playerRepository := new(mocks.IPlayerRepository)
 
-	player1 := models.PlayerModel{}
-	player1.Id = 101
-	player1.Name = "Rafael"
-	player1.Score = 3
+	player1 := models.PlayerModel{
+		Id:    101,
+		Name:  "Rafael",
+		Score: 3,
+	}
 
-	player2 := models.PlayerModel{}
-	player2.Id = 103
-	player2.Name = "Serena"
-	player2.Score = 1
-
+	player2 := models.PlayerModel{
+		Id:    103,
+		Name:  "Serena",
+		Score: 1,
+	}
 	playerRepository.On("GetPlayerByName", "Rafael").Return(player1, nil)
 	playerRepository.On("GetPlayerByName", "Serena").Return(player2, nil)
 
@@ -30,7 +30,40 @@ func TestGetScore(t *testing.T) {
 
 	expectedResult := "Forty-Fifteen"
 
-	actualResult, _ := playerService.GetScores("Rafael", "Serena")
+	actualResult, err := playerService.GetScores("Rafael", "Serena")
+	if err != nil {
+		t.Errorf("unexpected err during score info, err %s", err)
+	}
+
+	assert.Equal(t, expectedResult, actualResult)
+}
+
+func TestFailing(t *testing.T) {
+	playerRepository := new(mocks.IPlayerRepository)
+
+	player1 := models.PlayerModel{
+		Id:    101,
+		Name:  "Rafael",
+		Score: 3,
+	}
+
+	player2 := models.PlayerModel{
+		Id:    103,
+		Name:  "Serena",
+		Score: 3,
+	}
+
+	playerRepository.On("GetPlayerByName", "Rafael").Return(player1, nil)
+	playerRepository.On("GetPlayerByName", "Serena").Return(player2, nil)
+	playerService := PlayerService{playerRepository}
+
+	// should be "Deuce" to not  fail
+	expectedResult := "Thirty-All"
+
+	actualResult, err := playerService.GetScores("Rafael", "Serena")
+	if err != nil {
+		t.Errorf("unexpected err during score info, err %s", err)
+	}
 
 	assert.Equal(t, expectedResult, actualResult)
 }
